2573b8237d4cc2bbafaa2b86f9cd30a09985ba02
Elson Otake <elsonotake@yahoo.com.br>
2022-12-13 14:57:02 -0300

    Add json payload method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def json_payload
+    return [] if request.raw_post.empty?
+
+    begin
+      HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
+    rescue
+      return []
+    end
+  end
 end
