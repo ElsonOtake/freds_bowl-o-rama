6174a03826b396948e4884d2e99ecee641c4da14
Elson Otake <elsonotake@yahoo.com.br>
2022-12-13 17:08:52 -0300

    Add check for frame different of array

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -7,12 +7,13 @@ class ScoresController < ApplicationController
     data.each_pair do |key, value|
       scores[key] = 0 # Initalize the player score with zero points 
       num_frames = value.size if num_frames == 0 # Initialize the number of frames with the number of frames of the first player
-      return render json: { error: 'Invalid data format.' }, status: :unprocessable_entity unless value.is_a?(Array)
+      return render json: { error: 'Invalid frames format.' }, status: :unprocessable_entity unless value.is_a?(Array)
       return render json: { error: 'More than ten frames.' }, status: :unprocessable_entity if value.size > 10
       return render json: { error: 'Different number of frames.' }, status: :unprocessable_entity if value.size != num_frames
       extra_points = Array.new(21, 1) # Array use to calculate the points. Weight for each throw. Add one to strikes and spares.
       num_of_throw = 0
       value.each_with_index do |frame, index|
+        return render json: { error: 'Invalid frame format.' }, status: :unprocessable_entity unless frame.is_a?(Array)
         return render json: { error: 'Wrong number of throws.' }, status: :unprocessable_entity if (frame.size > 2 && index != 9) ||
           frame.size > 3
         return render json: { error: 'Invalid number of pins.' }, status: :unprocessable_entity if frame.any? { |pin| !pin.is_a?(Integer)}
